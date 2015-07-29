0c45c7bec122eea948c84beb4ccb356ffb14b1b6
gongbaochicken <jiazhuo0528@yahoo.com>
2015-07-29 13:01:56 -0400

    Add quickSort

---
+++ Basic Algorithms/quickSort.cpp
@@ -0,0 +1,53 @@
+//  QuickSort
+//  Created by Jason Zhuo Jia on 7/27/15.
+//
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int partition(vector<int>& nums, int front, int end){
+    int pivot = nums[front];
+    while(front < end){
+        while(nums[front] < pivot){
+            front++;
+        }
+        while(nums[end] > pivot){
+            end--;
+        }
+        swap(nums[front], nums[end]);
+    }
+    return front;
+}
+
+void quickSortHelper(vector<int>& nums, int front, int end){
+    if(front > end) return;
+    int pivot = partition(nums, front, end);
+    quickSortHelper(nums, front, pivot-1);
+    quickSortHelper(nums, pivot+1, end);
+}
+
+void quickSort(vector<int>& nums){
+    int len = (int) nums.size();
+    if(len == 0) return;
+    quickSortHelper(nums, 0, len-1);
+}
+
+
+/****************Test for Quick Sort********************/
+void printV(vector<int>& ve){
+    for (int j = 0; j < ve.size(); j++) {
+        printf("%d ", ve[j]);
+    }
+    printf("\n");
+}
+
+int main() {
+    vector<int> v1={1,9,5,7,2,6};
+    printV(v1);
+    quickSort(v1);
+    printV(v1);
+    
+    return 0;
+}
+
